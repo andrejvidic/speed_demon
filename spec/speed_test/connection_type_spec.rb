88d0f61d5ab5cdf8864ac2e824b908269bc9ae6d
Andre Vidic <andre@dtcorp.com.au>
2018-08-26 23:25:26 +1000

    ConnectionType-TEST-wireless? method returns false when on network

--- spec/speed_test/connection_type_spec.rb
+++ spec/speed_test/connection_type_spec.rb
@@ -45,4 +45,21 @@ describe SpeedTest::ConnectionType do
     end
   end
 
+  describe '#wireless?' do
+    let(:ip_link) { '1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+2: enp58s0f1: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc pfifo_fast state UP mode DEFAULT group default qlen 1000
+    link/ether 80:fa:5b:46:a2:9f brd ff:ff:ff:ff:ff:ff
+3: wlp59s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc mq state DOWN mode DORMANT group default qlen 1000
+    link/ether 00:28:f8:aa:b4:64 brd ff:ff:ff:ff:ff:ff' }
+    let(:connection) { described_class.new }
+
+    before do
+      allow_any_instance_of(described_class).to receive(:system).with('ip link').and_return(ip_link)
+    end
+
+    it 'returns false when connection is network' do
+      expect(connection.wireless?).to be(false)
+    end
+  end
 end

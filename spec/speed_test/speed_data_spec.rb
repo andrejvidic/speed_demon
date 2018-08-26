90e52ea42f14719ccc83478ebabd79db0628f9d6
Andre Vidic <andre@dtcorp.com.au>
2018-08-27 09:08:05 +1000

    SpeedData-TEST-Rename method speed_info to info

--- spec/speed_test/speed_data_spec.rb
+++ spec/speed_test/speed_data_spec.rb
@@ -3,27 +3,27 @@ require 'spec_helper'
 describe SpeedTest::SpeedData do
   describe 'upon initialization, 'do
     before do
-      allow_any_instance_of(described_class).to receive(:speed_info).and_return(speed_info)
+      allow_any_instance_of(described_class).to receive(:info).and_return(info)
     end
 
-    let(:speed_info) { 'speed info '}
+    let(:info) { 'speed info '}
     let(:speed) { described_class.new}
 
-    it 'calls speed_info' do
-      expect(speed.speed_info).to eq(speed_info)
+    it 'calls info' do
+      expect(speed.info).to eq(info)
     end
   end
 
   describe 'during initialization' do
     let(:speed) { described_class.new }
-    let(:speed_info) { 'speed info '}
+    let(:info) { 'speed info '}
 
     before do
-      allow_any_instance_of(described_class).to receive(:system).with('speedtest-cli --simple').and_return(speed_info)
+      allow_any_instance_of(described_class).to receive(:system).with('speedtest-cli --simple').and_return(info)
     end
 
     it 'calls command line speedtest-cli' do
-      expect(speed.speed_info).to eq(speed_info)
+      expect(speed.info).to eq(info)
     end
   end
 end

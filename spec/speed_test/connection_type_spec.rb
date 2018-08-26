8246fd88210c818de8735680710b05fa9e8a5d5f
Andre Vidic <andre@dtcorp.com.au>
2018-08-26 23:13:17 +1000

    ConnectionType-TEST-Linux 'ip link' is called

--- spec/speed_test/connection_type_spec.rb
+++ spec/speed_test/connection_type_spec.rb
@@ -13,4 +13,17 @@ describe SpeedTest::ConnectionType do
       expect(connection.connection_info).to eq(ip_link)
     end
   end
+
+  describe 'during intialization, ' do
+    let(:connection) { described_class.new }
+    let(:ip_link) { 'connection_info_string' }
+
+    before do
+      allow_any_instance_of(described_class).to receive(:system).with('ip link').and_return(ip_link)
+    end
+
+    it 'calls linux ip link' do
+      expect(connection.connection_info).to eq(ip_link)
+    end
+  end
 end

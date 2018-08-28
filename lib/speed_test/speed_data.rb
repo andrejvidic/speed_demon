2a6d7c817b62353ac5aad46cc64b0ff49d3e68a9
Andre Vidic <andre@dtcorp.com.au>
2018-08-28 22:15:17 +1000

    SpeedData-download method returns the download value in a string

--- lib/speed_test/speed_data.rb
+++ lib/speed_test/speed_data.rb
@@ -13,9 +13,17 @@ module SpeedTest
       ping_string.split(' ')[1]
     end
 
+    def download
+      download_string.split(' ')[1]
+    end
+
     private
     def ping_string
       info[0]
+    end
+
+    def download_string
+      info[1]
     end
   end
 end

49f2605eb1794beb0b3cfc69273409d2a6dcf4db
Andre Vidic <andre@dtcorp.com.au>
2018-08-28 21:59:30 +1000

    SpeedData-TEST-Expect info to produce an array instead of a string

--- spec/speed_test/speed_data_spec.rb
+++ spec/speed_test/speed_data_spec.rb
@@ -16,14 +16,15 @@ describe SpeedTest::SpeedData do
 
   describe 'during initialization' do
     let(:speed) { described_class.new }
-    let(:info) { 'speed info '}
+    let(:info) { 'Ping: 5.036ms\nDownload: 36.31 Mbit/s\nUpload: 5.43 Mbit/s'}
+    let(:info_array) { ['Ping: 5.036ms', 'Download: 36.31 Mbit/s', 'Upload: 5.43 Mbit/s'] }
 
     before do
       allow_any_instance_of(described_class).to receive(:system).with('speedtest-cli --simple').and_return(info)
     end
 
     it 'calls command line speedtest-cli' do
-      expect(speed.info).to eq(info)
+      expect(speed.info).to eq(info_array)
     end
   end
 end

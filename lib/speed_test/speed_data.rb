7697682c520c1aa4f7ed93ed091248ff238f72d2
Andre Vidic <andre@dtcorp.com.au>
2018-08-28 22:13:51 +1000

    SpeedData-ping method returns the ping value in a string

--- lib/speed_test/speed_data.rb
+++ lib/speed_test/speed_data.rb
@@ -2,10 +2,20 @@ module SpeedTest
   class SpeedData
     def initialize
       @info = info
+      @ping = ping
     end
 
     def info
       system('speedtest-cli --simple').split('\n')
+    end
+
+    def ping
+      ping_string.split(' ')[1]
+    end
+
+    private
+    def ping_string
+      info[0]
     end
   end
 end

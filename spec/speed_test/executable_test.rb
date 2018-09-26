98b218cd56101e1f90a9a4a7c4be3c8777e569a8
Andre Vidic <andre@dtcorp.com.au>
2018-09-27 08:45:20 +1000

    Executable-TEST-speedtest/output directory doesnt exist, create it

--- spec/speed_test/executable_test.rb
+++ spec/speed_test/executable_test.rb
@@ -2,6 +2,25 @@ require 'spec_helper'
 require 'fileutils'
 
 describe 'Run command line executable speedtest_init,' do
+  describe 'if speedtest/output directory does not exist,' do
+    describe 'create directory to current working directory' do
+      let (:dir) { Dir.pwd }
+      let (:file) { 'speedtest/output' }
+      let(:output_test_path) { File.join(dir, file) }
+
+      after do
+        # cleanup
+        dirs = ['speedtest', 'speedtest/output', 'speedtest/cron_logs']
+        dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir)}
+      end
+
+      it 'creates a speedtest directory' do
+        system('speedtest_init')
+        expect(File.directory?(output_test_path)).to be true
+      end
+    end
+  end
+
   describe 'if speedtest directory already exists,' do
     describe 'print warning message,' do
       let (:create_existing_directory) { FileUtils.mkdir_p(File.join(Dir.pwd, 'speedtest')) }

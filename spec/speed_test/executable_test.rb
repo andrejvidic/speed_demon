de29736fde66fa7e1c3c1936beeb3cf119439d3b
Andre Vidic <andre@dtcorp.com.au>
2018-09-28 11:48:50 +1000

    Executable-TEST-DRY up tests & delete repeated (covered) behaviour

--- spec/speed_test/executable_test.rb
+++ spec/speed_test/executable_test.rb
@@ -53,25 +53,6 @@ describe 'Run command line executable speedtest_init,' do
     end
   end
 
-  describe 'if speedtest/cron_logs directory does not exist,' do
-    describe 'create directory to current working directory' do
-      let (:dir) { Dir.pwd }
-      let (:file) { 'speedtest/cron_logs' }
-      let(:cron_test_path) { File.join(dir, file) }
-
-      after do
-        # cleanup
-        dirs = ['speedtest', 'speedtest/output', 'speedtest/cron_logs']
-        dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir)}
-      end
-
-      it 'creates a speedtest directory' do
-        system('speedtest_init')
-        expect(File.directory?(cron_test_path)).to be true
-      end
-    end
-  end
-
   describe 'if speedtest/output directory already exists,' do
     describe 'print warning message,' do
       let (:create_existing_directory) { FileUtils.mkdir_p(File.join(Dir.pwd, 'speedtest/output')) }
@@ -97,25 +78,6 @@ describe 'Run command line executable speedtest_init,' do
     end
   end
 
-  describe 'if speedtest/output directory does not exist,' do
-    describe 'create directory to current working directory' do
-      let (:dir) { Dir.pwd }
-      let (:file) { 'speedtest/output' }
-      let(:output_test_path) { File.join(dir, file) }
-
-      after do
-        # cleanup
-        dirs = ['speedtest', 'speedtest/output', 'speedtest/cron_logs']
-        dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir)}
-      end
-
-      it 'creates a speedtest directory' do
-        system('speedtest_init')
-        expect(File.directory?(output_test_path)).to be true
-      end
-    end
-  end
-
   describe 'if speedtest directory already exists,' do
     describe 'print warning message,' do
       let (:create_existing_directory) { FileUtils.mkdir_p(File.join(Dir.pwd, 'speedtest')) }
@@ -137,25 +99,6 @@ describe 'Run command line executable speedtest_init,' do
         expect { system('speedtest_init') }
           .to output(include("speedtest exists, not created\n"))
           .to_stderr_from_any_process
-      end
-    end
-  end
-
-  describe 'if speedtest directory does not exist,' do
-    describe 'create directory to current working directory' do
-      let (:dir) { Dir.pwd }
-      let (:file) { 'speedtest' }
-      let(:speed_test_path) { File.join(dir, file) }
-
-      after do
-        # cleanup
-        dirs = ['speedtest', 'speedtest/output', 'speedtest/cron_logs']
-        dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir)}
-      end
-
-      it 'creates a speedtest directory' do
-        system('speedtest_init')
-        expect(File.directory?(speed_test_path)).to be true
       end
     end
   end

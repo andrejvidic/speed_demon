70105b84c54ca1b73b9bdcfb01127ce26f59fe46
Andre Vidic <andre@dtcorp.com.au>
2018-09-28 11:47:20 +1000

    Executable-TEST-speedtest_init works in any chosen current directory

--- spec/speed_test/executable_test.rb
+++ spec/speed_test/executable_test.rb
@@ -2,6 +2,32 @@ require 'spec_helper'
 require 'fileutils'
 
 describe 'Run command line executable speedtest_init,' do
+  describe 'allowing a user to navigate to any directory, /tmp directory for example,' do
+    describe 'and create all speedtest directories at this location' do
+      let (:base_dir) { '/tmp' }
+      let (:dirs) { ['speedtest', 'speedtest/cron_logs', 'speedtest/output'] }
+      let (:set_current_directory) { Dir.chdir(base_dir) }
+
+      before do
+        set_current_directory
+      end
+
+      after do
+        # cleanup
+        dirs = ['speedtest', 'speedtest/output', 'speedtest/cron_logs']
+        dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir)}
+      end
+
+      it 'creates a speedtest directory' do
+        system('speedtest_init')
+        dirs.each do |dir|
+          path = File.join(base_dir, dir)
+          expect(File.directory?(path)).to be true
+        end
+      end
+    end
+  end
+
   describe 'if speedtest/cron_logs directory already exists,' do
     describe 'print warning message,' do
       let (:create_existing_directory) { FileUtils.mkdir_p(File.join(Dir.pwd, 'speedtest/cron_logs')) }

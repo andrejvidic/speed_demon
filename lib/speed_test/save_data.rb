67d482fe8dbd3871a50a4d8dcd76adb4649891f0
Andre Vidic <andre@dtcorp.com.au>
2018-10-03 07:55:18 +1000

    Save_data-first line of the csv is a header

--- lib/speed_test/save_data.rb
+++ lib/speed_test/save_data.rb
@@ -5,11 +5,19 @@ module SpeedTest
   class SaveData
     def initialize(file)
       @csv_file_name = file
-      create_csv
     end
 
     def create_csv
-      CSV.open(@csv_file_name, 'a+', headers: true) do |csv|
+      CSV.open(@csv_file_name, 'a+', headers: true) do |line|
+        # Write headers only if the file is new (empty)
+        if line.eof?
+          headers = ['Time',
+                     'Ping (ms)',
+                     'Download Speed (Mbit/s)',
+                     'Upload Speed (Mbit/s)',
+                     'wireless connection?']
+          line << headers
+        end
       end
     end
   end

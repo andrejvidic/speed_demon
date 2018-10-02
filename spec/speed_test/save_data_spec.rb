17e57908f3be40ecbc77af71c7e73aee9149d2c0
Andre Vidic <andre@dtcorp.com.au>
2018-10-03 08:33:24 +1000

    Save_data-TEST-correctly writes data to csv on 2nd line

--- spec/speed_test/save_data_spec.rb
+++ spec/speed_test/save_data_spec.rb
@@ -17,15 +17,30 @@ describe SpeedTest::SaveData do
     end
 
     it 'creates a csv file in the specified output directory' do
-      SpeedTest::SaveData.new(csv).create_csv
+      SpeedTest::SaveData.new(csv)
       expect(File.exist?(csv)).to be true
     end
 
     it 'the first line of the csv is a header' do
-      SpeedTest::SaveData.new(csv).create_csv
+      SpeedTest::SaveData.new(csv)
       csv_first_line = IO.readlines(csv)[0]
       header_string = "Time,Ping (ms),Download Speed (Mbit/s),Upload Speed (Mbit/s),wireless connection?\n"
       expect(csv_first_line).to eq(header_string)
+    end
+
+    it 'correctly writes data to csv on 2nd line' do
+      time = Time.now
+      ping = 10
+      download = 45
+      upload = 20
+      SpeedTest::SaveData.new(csv).save({time: time,
+                                         ping: ping,
+                                         download: download,
+                                         upload: upload,
+                                         wireless?: false })
+      csv_second_line = IO.readlines(csv)[1]
+      data_string = "#{time},#{ping},#{download},#{upload},false\n"
+      expect(csv_second_line).to eq(data_string)
     end
   end
 end

c1db0b3f1987561b2d8b5e13a62dda8f653c446c
Andre Vidic <andre@dtcorp.com.au>
2018-10-11 08:25:19 +1100

    Setup-Rename schedule to cron & cron_log to log

--- spec/speed_test/setup_spec.rb
+++ spec/speed_test/setup_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe SpeedTest::Setup do
     let (:base_dir) { '/tmp' }
     let (:speedtest) { "#{base_dir}/speedtest"}
     let (:output_dir) { "#{speedtest}/output" }
-    let (:cron_log_dir) { "#{speedtest}/log" }
-    let (:schedule_dir) { "#{speedtest}/schedule" }
-    let (:dirs) { [speedtest, output_dir, cron_log_dir, schedule_dir] }
-    let (:custom) { { output: output_dir, log: cron_log_dir } }
+    let (:log_dir) { "#{speedtest}/log" }
+    let (:cron_dir) { "#{speedtest}/cron" }
+    let (:dirs) { [speedtest, output_dir, log_dir, cron_dir] }
+    let (:custom) { { output: output_dir, log: log_dir } }
 
     before do
       Dir.chdir(base_dir) # set_current_directory
@@ -24,34 +24,34 @@ RSpec.describe SpeedTest::Setup do
       expect(File.directory?(output_dir)).to be true
     end
 
-    it 'creates cron_log directory at /tmp/log' do
-      expect(File.directory?(cron_log_dir)).to be true
+    it 'creates log directory at /tmp/log' do
+      expect(File.directory?(log_dir)).to be true
     end
 
-    it 'creates the schedule directory at /tmp/schedule' do
-      expect(File.directory?(schedule_dir)).to be true
+    it 'creates the cron directory at /tmp/cron' do
+      expect(File.directory?(cron_dir)).to be true
     end
   end
 
   describe 'calling cron methods,' do
     let (:base_dir) { '/tmp' }
     let (:speedtest) { "#{base_dir}/speedtest"}
-    let (:schedule_dir) { "#{speedtest}/schedule"}
-    let (:schedule_file) { "#{schedule_dir}/schedule.rb"}
+    let (:cron_dir) { "#{speedtest}/cron"}
+    let (:cron_file) { "#{cron_dir}/cron.rb"}
     let (:output_dir) { "#{base_dir}/output" }
-    let (:cron_log_dir) { "#{base_dir}/log" }
-    let (:dirs) { [speedtest, output_dir, cron_log_dir, schedule_dir] }
-    let (:options) { { base_dir: base_dir, custom: { output: output_dir, log: cron_log_dir } } }
+    let (:log_dir) { "#{base_dir}/log" }
+    let (:dirs) { [speedtest, output_dir, log_dir, cron_dir] }
+    let (:options) { { base_dir: base_dir, custom: { output: output_dir, log: log_dir } } }
     let (:whenever) { 'whenever gem called' }
     let (:setup) { SpeedTest::Setup.new(options) }
     let (:default_frequency) { '15.minutes' }
-    let (:default_schedule_file_contents) do
+    let (:default_cron_file_contents) do
 <<FILE
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 #
 
-set :output, "#{cron_log_dir}/cron.log"
+set :output, "#{log_dir}/cron.log"
 
 every #{default_frequency} do
   command 'ruby speed_test_check'
@@ -71,27 +71,27 @@ FILE
     describe 'cron_start' do
       it 'adds and starts the cron_task by calling the whenever gem' do
         allow_any_instance_of(SpeedTest::Setup).to receive(:system)
-          .with("whenever --load-file #{schedule_file}")
+          .with("whenever --load-file #{cron_file}")
           .and_return(whenever)
         expect(setup.cron_start).to eq(whenever)
       end
     end
 
     describe 'cron_create' do
-      it 'creates the schedule file' do
+      it 'creates the cron file' do
         setup.cron_create
-        expect(File.exist?(schedule_file)).to be true
+        expect(File.exist?(cron_file)).to be true
       end
 
       it 'has the correct default contents' do
         setup.cron_create
-        expect(File.read(schedule_file)).to eq(default_schedule_file_contents)
+        expect(File.read(cron_file)).to eq(default_cron_file_contents)
       end
 
       describe 'after calling the whenever gem,' do
-        it 'creates the schedule directory' do
+        it 'creates the cron directory' do
           setup.cron_create
-          expect(File.directory?(schedule_dir)).to be true
+          expect(File.directory?(cron_dir)).to be true
         end
       end
     end

16ecc151b2b2ed02e144e4263968edc46a13958f
Andre Vidic <andre@dtcorp.com.au>
2018-10-13 18:52:44 +1100

    SpeedTest-Add cli file to project

--- lib/speed_test.rb
+++ lib/speed_test.rb
@@ -3,6 +3,7 @@ require_relative 'speed_test/connection_type'
 require_relative 'speed_test/speed_data'
 require_relative 'speed_test/save_data'
 require_relative 'speed_test/setup'
+require_relative 'speed_test/cli'
 require_relative 'speed_test/version'
 
 module SpeedTest

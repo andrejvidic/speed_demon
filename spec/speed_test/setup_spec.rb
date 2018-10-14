3563e69a985b05c38d90f3328a220b5142a27746
Andre Vidic <andre@dtcorp.com.au>
2018-10-14 21:45:32 +1100

    Setup-TEST-Move default directories to linux/unix standards

--- spec/speed_test/setup_spec.rb
+++ spec/speed_test/setup_spec.rb
@@ -2,47 +2,16 @@ require 'spec_helper'
 require 'fileutils'
 
 RSpec.describe SpeedTest::Setup do
-  describe 'calling directories,' do
+  describe 'calling execute with default options,' do
     let (:base_dir) { '/tmp' }
-    let (:speedtest) { "#{base_dir}/speedtest"}
-    let (:output_dir) { "#{speedtest}/output" }
-    let (:log_dir) { "#{speedtest}/log" }
-    let (:cron_dir) { "#{speedtest}/cron" }
+    let (:speedtest) { "#{base_dir}/speedtest" }
+    let (:output_dir) { "~/.local/share/speedtest/output" }
+    let (:log_dir) { "~/.speedtest/log" }
+    let (:cron_dir) { "~/.config/speedtest/cron" }
+    let (:cron_file) { "#{cron_dir}/cron.rb" }
     let (:dirs) { [speedtest, output_dir, log_dir, cron_dir] }
-    let (:options) { { base_dir: base_dir, output: output_dir, log: log_dir } }
-    before do
-      Dir.chdir(base_dir) # set_current_directory
-      SpeedTest::Setup.new(options).directories # create_directories
-    end
-
-    after do
-      dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir) } # cleanup
-    end
-
-    it 'creates output directory at /tmp/output' do
-      expect(File.directory?(output_dir)).to be true
-    end
-
-    it 'creates log directory at /tmp/log' do
-      expect(File.directory?(log_dir)).to be true
-    end
-
-    it 'creates the cron directory at /tmp/cron' do
-      expect(File.directory?(cron_dir)).to be true
-    end
-  end
-
-  describe 'calling cron methods,' do
-    let (:base_dir) { '/tmp' }
-    let (:speedtest) { "#{base_dir}/speedtest"}
-    let (:cron_dir) { "#{speedtest}/cron"}
-    let (:cron_file) { "#{cron_dir}/cron.rb"}
-    let (:output_dir) { "#{base_dir}/output" }
-    let (:log_dir) { "#{base_dir}/log" }
-    let (:dirs) { [speedtest, output_dir, log_dir, cron_dir] }
-    let (:options) { { base_dir: base_dir, output: output_dir, log: log_dir } }
+    let (:options) { { base_dir: base_dir, output: output_dir, log: log_dir, cron: cron_dir } }
     let (:whenever) { 'whenever gem called' }
-    let (:setup) { SpeedTest::Setup.new(options) }
     let (:default_frequency) { '15.minutes' }
     let (:default_cron_file_contents) do
 <<FILE
@@ -59,7 +28,8 @@ FILE
     end
 
     before do
-      setup.directories
+      Dir.chdir(base_dir) # set_current_directory
+      SpeedTest::Setup.execute(options)
     end
 
     after do
@@ -67,32 +37,27 @@ FILE
       dirs.each { |dir| FileUtils.rm_rf(dir) if File.directory?(dir) }
     end
 
-    describe 'cron_start' do
-      it 'adds and starts the cron_task by calling the whenever gem' do
-        allow_any_instance_of(SpeedTest::Setup).to receive(:system)
-          .with("whenever --load-file #{cron_file}")
-          .and_return(whenever)
-        expect(setup.cron_start).to eq(whenever)
-      end
+    it 'creates the default output directory at speedtest/output' do
+      expect(File.directory?(output_dir)).to be true
     end
 
-    describe 'cron_create' do
-      it 'creates the cron file' do
-        setup.cron_create
-        expect(File.exist?(cron_file)).to be true
-      end
+    it 'creates the default custom log directory at speedtest/log' do
+      expect(File.directory?(log_dir)).to be true
+    end
 
-      it 'has the correct default contents' do
-        setup.cron_create
-        expect(File.read(cron_file)).to eq(default_cron_file_contents)
-      end
+    it 'creates the default cron directory at speedtest/cron' do
+      expect(File.directory?(cron_dir)).to be true
+    end
 
-      describe 'after calling the whenever gem,' do
-        it 'creates the cron directory' do
-          setup.cron_create
-          expect(File.directory?(cron_dir)).to be true
-        end
-      end
+    it 'adds and starts the cron_task by calling the whenever gem' do
+      allow_any_instance_of(SpeedTest::Setup).to receive(:system)
+        .with("whenever --load-file #{cron_file}")
+        .and_return(whenever)
+    end
+
+    it 'creates the cron file with correct defaults' do
+      expect(File.exist?(cron_file)).to be true
+      expect(File.read(cron_file)).to eq(default_cron_file_contents)
     end
   end
 end

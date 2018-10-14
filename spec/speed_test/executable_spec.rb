86ccf769836d2e8d3996c7166feb6621efa50850
Andre Vidic <andre@dtcorp.com.au>
2018-10-14 16:14:27 +1100

    Executable-TEST-Passing a frequency using CLI flag updates cron.rb

--- spec/speed_test/executable_spec.rb
+++ spec/speed_test/executable_spec.rb
@@ -15,16 +15,34 @@ describe 'Run command line executable speedtest_init,' do
     end
   end
 
-  describe 'allowing a user to specify location of all directories,' do
+  describe 'allow a user to specify CLI options,' do
     let (:base_dir) { '/tmp' }
     let (:speedtest) { "#{base_dir}/speedtest"}
-    let (:output_dir) { "#{base_dir}/output" }
-    let (:log_dir) { "#{base_dir}/log" }
-    let (:cron_dir) { "#{base_dir}/cron" }
-    let (:dirs) { [speedtest, output_dir, log_dir] }
-    let (:options) { { base_dir: base_dir, custom: { output: output_dir, log: log_dir, cron: cron_dir } } }
+    let (:custom_output_dir) { "#{base_dir}/output" }
+    let (:custom_log_dir) { "#{base_dir}/log" }
+    let (:custom_cron_dir) { "#{base_dir}/cron" }
+    let (:custom_cron_file) { "#{custom_cron_dir}/cron.rb"}
+    let (:dirs) { [speedtest, custom_output_dir, custom_log_dir, custom_cron_dir] }
+    let (:options) { { base_dir: base_dir,
+                       output: custom_output_dir,
+                       log: custom_log_dir,
+                       cron: custom_cron_dir } }
     let (:setup) { SpeedTest::Setup.new(options) }
     let (:set_current_directory) { Dir.chdir(base_dir) }
+    let (:frequency_1hr) { ':hour' }
+    let (:custom_cron_file_contents) do
+<<FILE
+# Use this file to easily define all of your cron jobs.
+# Learn more: http://github.com/javan/whenever
+#
+
+set :output, "#{custom_log_dir}/cron.log"
+
+every #{frequency_1hr} do
+  command 'ruby speed_test_check'
+end
+FILE
+    end
 
     before do
       set_current_directory
@@ -37,18 +55,25 @@ describe 'Run command line executable speedtest_init,' do
 
     describe 'create directories at locations other than /tmp/speedtest' do
       it 'creates output directory at /tmp/output' do
-        system("speedtest_init --output #{output_dir}")
-        expect(File.directory?(output_dir)).to be true
+        system("speedtest_init --output #{custom_output_dir}")
+        expect(File.directory?(custom_output_dir)).to be true
       end
 
       it 'creates log directory at /tmp/log' do
-        system("speedtest_init --log #{log_dir}")
-        expect(File.directory?(log_dir)).to be true
+        system("speedtest_init --log #{custom_log_dir}")
+        expect(File.directory?(custom_log_dir)).to be true
       end
 
       it 'creates cron directory at /tmp/cron' do
-        system("speedtest_init --cron #{cron_dir}")
-        expect(File.directory?(cron_dir)).to be true
+        system("speedtest_init --cron #{custom_cron_dir}")
+        expect(File.directory?(custom_cron_dir)).to be true
+      end
+    end
+
+    describe 'specify frequency at which speed is measured & saved' do
+      it 'overrides default "15.minutes" to every hour (:hour)' do
+        system("speedtest_init --frequency #{frequency_1hr} --cron #{custom_cron_dir} --log #{custom_log_dir}")
+        expect(File.read(custom_cron_file)).to eq(custom_cron_file_contents)
       end
     end
   end

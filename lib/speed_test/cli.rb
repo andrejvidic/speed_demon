60960e259138ad6178579883592c267806f89acc
Andre Vidic <andre@dtcorp.com.au>
2018-10-16 07:56:29 +1100

    CLI-Add --measure-speed flag, remove default, rename setup_option

--- lib/speed_test/cli.rb
+++ lib/speed_test/cli.rb
@@ -13,10 +13,11 @@ module SpeedTest
     end
 
     class ScriptOptions
-      attr_accessor :default, :output, :log, :cron, :frequency, :setup_options
+      attr_accessor :setup, :measure, :output, :log, :cron, :frequency
 
       def initialize
-        self.setup_options = false
+        self.setup = false
+        self.measure = false
       end
 
       def define_options(parser)
@@ -25,6 +26,7 @@ module SpeedTest
         display_help(parser)
 
         # CLI parse options
+        measure_speed(parser)
         setup_default(parser)
         custom_output_path(parser)
         custom_log_path(parser)
@@ -39,38 +41,43 @@ module SpeedTest
         end
       end
 
+      def measure_speed(parser)
+        parser.on('-m', '--measure-speed', TrueClass, 'Measure internet speed and save it') do |path|
+          self.measure = true          
+        end
+      end
+
       def setup_default(parser)
-        parser.on('-s', '--setup-default', TrueClass, 'specify true/false, yes/no to setup speedtest to default directories') do |boolean|
-          self.default = boolean
-          self.setup_options = true
+        parser.on('-s', '--setup-default', TrueClass, 'Setup speedtest with default directories') do |boolean|
+          self.setup = true
         end
       end
 
       def custom_output_path(parser)
-        parser.on('-o', '--custom-output PATH', String, 'specify custom path for output directory') do |path|
+        parser.on('-o', '--custom-output PATH', String, 'Override the default output directory with custom') do |path|
           self.output = path
-          self.setup_options = true          
+          self.setup = true
         end
       end
 
       def custom_log_path(parser)
-        parser.on('-l', '--custom-log PATH', String, 'specify custom path for log directory') do |path|
+        parser.on('-l', '--custom-log PATH', String, 'Override the default log directory with custom') do |path|
           self.log = path
-          self.setup_options = true          
+          self.setup = true
         end
       end
 
       def custom_cron_path(parser)
-        parser.on('-c', '--custom-cron PATH', String, 'specify custom path for cron directory') do |path|
+        parser.on('-c', '--custom-cron PATH', String, 'Override the default cron directory with custom') do |path|
           self.cron = path
-          self.setup_options = true          
+          self.setup = true
         end
       end
 
       def custom_frequency(parser)
-        parser.on('-f', '--custom-frequency TIME', String, 'specify custom speedtest measuring frequency') do |time|
+        parser.on('-f', '--custom-frequency TIME', String, 'Override the default measuring frequency with custom') do |time|
           self.frequency = time
-          self.setup_options = true
+          self.setup = true
         end
       end
     end

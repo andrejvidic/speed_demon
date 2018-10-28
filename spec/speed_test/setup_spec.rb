abed73b07855c93802d2b658c2ab9bd4e0ebb1ce
Andre Vidic <andre@dtcorp.com.au>
2018-10-28 12:57:13 +1100

    Setup-TEST-Mock out all update-crontab (write) calls

--- spec/speed_test/setup_spec.rb
+++ spec/speed_test/setup_spec.rb
@@ -3,11 +3,11 @@ require 'fileutils'
 
 RSpec.describe SpeedTest::Setup do
   describe 'calling execute with default options,' do
-    let (:default_output_dir) { File.expand_path("~/.local/share/speedtest") }
-    let (:default_log_dir) { File.expand_path("~/.speedtest") }
-    let (:default_config_dir) { File.expand_path("~/.config/speedtest") }
-    let (:default_cron_file) { File.expand_path("#{default_config_dir}/cron.rb") }
-    let (:dirs) { [default_output_dir, default_log_dir, default_config_dir] }
+    let (:output_dir) { File.expand_path("~/.local/share/speedtest") }
+    let (:log_dir) { File.expand_path("~/.speedtest") }
+    let (:config_dir) { File.expand_path("~/.config/speedtest") }
+    let (:cron_schedule_file) { File.expand_path("#{config_dir}/cron.rb") }
+    let (:dirs) { [output_dir, log_dir, config_dir] }
     let (:path) {`echo $PATH`}
     class MockCli
       attr_reader :output, :log, :frequency
@@ -20,7 +20,7 @@ RSpec.describe SpeedTest::Setup do
     let (:cli) { MockCli.new }
     let (:whenever) { 'whenever gem called' }
     let (:default_frequency) { '15.minutes' }
-    let (:default_cron_file_contents) do
+    let (:cron_schedule_file_contents) do
 <<FILE
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
@@ -35,7 +35,10 @@ FILE
     end
 
     before do
-      SpeedTest::Setup.execute(cli)
+      allow_any_instance_of(described_class).to receive(:system)
+        .with("whenever --update-crontab --load-file  #{cron_schedule_file}")
+        .and_return(whenever)
+      described_class.execute(cli)
     end
 
     after do
@@ -44,26 +47,26 @@ FILE
     end
 
     it 'creates the default output directory at ~/.local/share/speedtest/output' do
-      expect(File.directory?(default_output_dir)).to be true
+      expect(File.directory?(output_dir)).to be true
     end
 
     it 'creates the default custom log directory at ~/.speedtest/log' do
-      expect(File.directory?(default_log_dir)).to be true
+      expect(File.directory?(log_dir)).to be true
     end
 
     it 'creates the default config directory at ~/.config/speedtest/' do
-      expect(File.directory?(default_config_dir)).to be true
+      expect(File.directory?(config_dir)).to be true
     end
 
     it 'adds and starts the cron_task by calling the whenever gem' do
       allow_any_instance_of(SpeedTest::Setup).to receive(:system)
         .with("whenever --load-file #{default_cron_file}")
         .and_return(whenever)
+    it 'creates the cron schedule file with correct defaults' do
+      expect(File.exist?(cron_schedule_file)).to be true
+      expect(File.read(cron_schedule_file)).to eq(cron_schedule_file_contents)
     end
 
-    it 'creates the cron file with correct defaults' do
-      expect(File.exist?(default_cron_file)).to be true
-      expect(File.read(default_cron_file)).to eq(default_cron_file_contents)
     end
   end
 end

d39b9c404e26507f091f5bc303d59713ec41942e
Andre Vidic <andre@dtcorp.com.au>
2018-10-30 08:41:44 +1100

    Settings-TEST-Change to use .create and .load

--- spec/speed_test/settings_spec.rb
+++ spec/speed_test/settings_spec.rb
@@ -3,32 +3,36 @@ require 'spec_helper'
 RSpec.describe SpeedTest::Settings do
   describe 'Setup has been completed and saved settings to yaml file,' do
     describe 'Settings.new is called' do
-      let (:settings_file) { "/tmp/settings.yaml" }
       let (:output) { "/tmp/output" }
       let (:log) { "/tmp/log" }
       let (:frequency) { "2.minutes" }
-      let (:settings_file_contents) do
-<<FILE
----
-output: #{output}
-log: #{log}
-frequency: #{frequency}
-FILE
-      end
-      let (:settings) { described_class.new(settings_file: settings_file,
-                                            output: output,
-                                            log: log,
-                                            frequency: frequency) }
-      it 'exposes output path via settings.output' do
-        expect(settings.output).to eq(output)
+      let (:settings_path) { "/tmp" }
+      let (:settings) { described_class.create(settings_path: settings_path,
+                                               output: output,
+                                               log: log,
+                                               frequency: frequency) }
+      it 'exposes output path via settings.load(settings_path).output' do
+        expect(described_class.load(settings_path).output).to eq(output)
       end
 
-      it 'exposes output path via settings.log' do
-        expect(settings.log).to eq(log)
+      it 'exposes output path via settings.load(settings_path).log' do
+        expect(described_class.load(settings_path).log).to eq(log)
       end
 
-      it 'exposes output path via settings.frequency' do
-        expect(settings.frequency).to eq(frequency)
+      it 'exposes output path via settings.load(settings_path).frequency' do
+        expect(described_class.load(settings_path).frequency).to eq(frequency)
+      end
+
+      it 'loads output settings given correct path' do
+        expect(described_class.load(settings_path).output).to eq(output)
+      end
+
+      it 'loads log settings given correct path' do
+        expect(described_class.load(settings_path).log).to eq(log)
+      end
+
+      it 'loads frequency settings given correct path' do
+        expect(described_class.load(settings_path).frequency).to eq(frequency)
       end
     end
   end

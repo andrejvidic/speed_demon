69119446bceac2d94031598713e28e2651560254
Andre Vidic <andre@dtcorp.com.au>
2018-10-31 19:24:59 +1100

    Settings-TEST-Create behavior

--- spec/speed_test/settings_spec.rb
+++ spec/speed_test/settings_spec.rb
@@ -13,6 +13,15 @@ RSpec.describe SpeedTest::Settings do
                              frequency: frequency)
     end
 
+    let (:settings_file_contents) do
+<<FILE
+---
+output: #{output}
+log: #{log}
+frequency: #{frequency}
+FILE
+    end
+
     before do
       FileUtils.mkdir_p(config) # make directory as this is usually done in SpeedTest::Setup
       create_settings_yaml
@@ -21,6 +30,15 @@ RSpec.describe SpeedTest::Settings do
     after do
       FileUtils.rm_rf(config) if File.directory?(config) # cleanup
     end
+
+    it 'creates a settings.yaml file assuming Setup creates the config path' do
+      expect(File.exist?("#{config}/settings.yaml")).to be true
+    end
+
+    it 'creates a settings.yaml file with the correct contents' do
+      expect(File.read("#{config}/settings.yaml")).to eq(settings_file_contents)
+    end
+
     it 'loads output settings given correct path' do
       expect(described_class.load(config).output).to eq(output)
     end

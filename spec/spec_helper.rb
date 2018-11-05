aed90e4575bbce9cf3c990e1945c04a91763b4ba
Andre Vidic <andre@dtcorp.com.au>
2018-11-05 11:50:47 +1100

    SpecHelper-Fix rubocop issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'speed_demon'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect

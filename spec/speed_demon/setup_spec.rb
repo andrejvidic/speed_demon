90d769e1b168137de3571a09c8b82eccf345d85b
Andre Vidic <andre@dtcorp.com.au>
2018-11-05 11:52:11 +1100

    Setup-TEST-Fix rubocop issues

--- spec/speed_demon/setup_spec.rb
+++ spec/speed_demon/setup_spec.rb
@@ -3,14 +3,14 @@ require 'fileutils'
 
 RSpec.describe SpeedDemon::Setup do
   describe 'calling execute with default options,' do
-    let (:output_dir) { File.expand_path("~/.local/share/speed_demon") }
-    let (:log_dir) { File.expand_path("~/.speed_demon") }
-    let (:config_dir) { File.expand_path("~/.config/speed_demon") }
-    let (:cron_schedule_file) { File.expand_path("#{config_dir}/cron.rb") }
-    let (:cron_log_file) { File.expand_path("#{log_dir}/cron.log") }
-    let (:dirs) { [output_dir, log_dir, config_dir] }
-    let (:timestamp_generator_file) { File.expand_path("#{config_dir}/timestamp_generator.sh") }
-    let (:path) {`echo $PATH`}
+    let(:output_dir) { File.expand_path("~/.local/share/speed_demon") }
+    let(:log_dir) { File.expand_path("~/.speed_demon") }
+    let(:config_dir) { File.expand_path("~/.config/speed_demon") }
+    let(:cron_schedule_file) { File.expand_path("#{config_dir}/cron.rb") }
+    let(:cron_log_file) { File.expand_path("#{log_dir}/cron.log") }
+    let(:dirs) { [output_dir, log_dir, config_dir] }
+    let(:timestamp_generator_file) { File.expand_path("#{config_dir}/timestamp_generator.rb") }
+    let(:path) { `echo $PATH` }
     class MockCli
       attr_reader :output, :log, :frequency
       def initialize
@@ -19,41 +19,37 @@ RSpec.describe SpeedDemon::Setup do
         @frequency = nil
       end
     end
-    let (:cli) { MockCli.new }
-    let (:whenever) { 'whenever gem called' }
-    let (:default_frequency) { '15.minutes' }
-    let (:cron_schedule_file_contents) do
-<<FILE
-# Use this file to easily define all of your cron jobs.
-# Learn more: http://github.com/javan/whenever
-#
+    let(:cli) { MockCli.new }
+    let(:whenever) { 'whenever gem called' }
+    let(:default_frequency) { '15.minutes' }
+    let(:cron_schedule_file_contents) do
+      p <<~RUBY
+      # Use this file to easily define all of your cron jobs.
+      # Learn more: http://github.com/javan/whenever
+      #
 
-job_type :call_executable, 'export PATH=#{path} && :task'
+      job_type :call_executable, 'export PATH=#{path} && :task'
 
-every #{default_frequency} do
-call_executable 'speed_demon -m 2>&1 | #{timestamp_generator_file} >> #{cron_log_file}'
-end
-FILE
+      every #{default_frequency} do
+        call_executable 'speed_demon -m 2>&1 | ./#{timestamp_generator_file} >> #{cron_log_file}'
+      end
+      RUBY
     end
 
-    let (:timestamp_generator_file_contents) do
-<<FILE
-#!/bin/bash
+    let(:timestamp_generator_file_contents) do
+      p <<~RUBY
+      #!/usr/bin/env ruby
 
-while read x; do
-    echo -n `date '+%Y-%m-%dT%T%z'`;
-    echo -n " ";
-    echo $x;
-done
-FILE
+      ARGF.each do |line|
+        puts "#{Time.now.strftime('%Y-%m-%dT%T%z')} " << line
+      end
+      RUBY
     end
 
-    let (:cron_log_file_contents) do
-<<FILE
-2018-10-28T17:05:15+1100 sh: 1: executable_that_doesnt_exist: not found
-FILE
+    let(:cron_log_file_contents) do
+      '2018-10-28T17:05:15+1100 sh: 1: executable_that_doesnt_exist: not found'
     end
-    let (:config_dir) { File.expand_path("~/.config/speed_demon") }
+    let(:config_dir) { File.expand_path("~/.config/speed_demon") }
 
     before do
       allow_any_instance_of(described_class).to receive(:system)
@@ -98,8 +94,10 @@ FILE
       cron_log_file_contents_array = File.read(cron_log_file).partition(' ')
       cron_log_file_time = cron_log_file_contents_array.first
       cron_log_file_error = cron_log_file_contents_array.last
-      expect{ DateTime.strptime(cron_log_file_time, '%Y-%m-%dT%T%z') }.to_not raise_error
-      expect(cron_log_file_error).to eq("sh: 1: executable_that_doesnt_exist: not found\n")
+      expect { DateTime.strptime(cron_log_file_time, '%Y-%m-%dT%T%z') }
+        .to_not raise_error
+      expect(cron_log_file_error)
+        .to eq("sh: 1: executable_that_doesnt_exist: not found\n")
     end
   end
 end

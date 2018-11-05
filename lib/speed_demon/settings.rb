78ebc5a8526f4f7e48ccd70a33cc6671c2bda069
Andre Vidic <andre@dtcorp.com.au>
2018-11-05 11:49:49 +1100

    Settings-Fix rubocop issues

--- lib/speed_demon/settings.rb
+++ lib/speed_demon/settings.rb
@@ -1,20 +1,21 @@
+require 'yaml'
+
 module SpeedDemon
+  # Create & load from persistent settings.yaml file
   class Settings
     attr_reader :output, :log, :config, :frequency
 
     def self.create(args)
-      settings = self.new(args)
+      settings = new(args)
       settings.create_settings_file
     end
 
     def self.load(config_dir)
       settings_file = File.expand_path("#{config_dir}/settings.yaml")
-      if File.exist?(settings_file)
-        load = YAML::load_file(settings_file)
-        new(output: load["output"], log: load["log"], frequency: load["frequency"], config: '')
-      else
-        raise LoadError.new("settings.yaml does not exist. Run setup")
-      end
+      raise LoadError, 'settings.yaml does not exist. Run setup' unless File.exist?(settings_file)
+
+      load = YAML.load_file(settings_file)
+      new(output: load[:output], log: load[:log], frequency: load[:frequency], config: '')
     end
 
     def initialize(args)
@@ -25,8 +26,8 @@ module SpeedDemon
     end
 
     def create_settings_file
-      File.open(settings_file_name, "w") do |file|
-        file.write(settings_file_contents).to_yaml
+      File.open(settings_file_name, 'w') do |file|
+        file.write(settings_file_yaml)
         file.close
       end
     end
@@ -37,13 +38,10 @@ module SpeedDemon
       File.expand_path("#{@config}/settings.yaml")
     end
 
-    def settings_file_contents
-<<FILE
----
-output: #{output}
-log: #{log}
-frequency: #{frequency}
-FILE
+    def settings_file_yaml
+      Hash[output: @output,
+           log: @log,
+           frequency: @frequency ].to_yaml
     end
   end
 end

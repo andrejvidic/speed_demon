488d2ca97a6fcb1ae763e19d3461086cf6a3393b
Andre Vidic <andre@dtcorp.com.au>
2018-11-05 11:51:12 +1100

    ConnectionType-TEST-Fix rubocop issues

--- spec/speed_demon/connection_type_spec.rb
+++ spec/speed_demon/connection_type_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe SpeedDemon::ConnectionType do
   describe 'upon intialization, ' do
     before do
-    allow_any_instance_of(described_class).to receive(:connection_info).and_return(ip_link)
+      allow_any_instance_of(described_class).to receive(:connection_info)
+        .and_return(ip_link)
     end
 
     let(:connection) { described_class.new }
@@ -28,12 +29,15 @@ describe SpeedDemon::ConnectionType do
   end
 
   describe '#wireless?' do
-    let(:ip_link) { '1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    let(:ip_link) do
+      '1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
     link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
 2: enp58s0f1: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc pfifo_fast state DOWN mode DEFAULT group default qlen 1000
     link/ether 80:fa:5b:46:a2:9f brd ff:ff:ff:ff:ff:ff
 3: wlp59s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc mq state UP mode DORMANT group default qlen 1000
-    link/ether 00:28:f8:aa:b4:64 brd ff:ff:ff:ff:ff:ff' }
+    link/ether 00:28:f8:aa:b4:64 brd ff:ff:ff:ff:ff:ff'
+    end
+
     let(:connection) { described_class.new }
 
     before do
@@ -46,12 +50,15 @@ describe SpeedDemon::ConnectionType do
   end
 
   describe '#wireless?' do
-    let(:ip_link) { '1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    let(:ip_link) do
+      '1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
     link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
 2: enp58s0f1: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc pfifo_fast state UP mode DEFAULT group default qlen 1000
     link/ether 80:fa:5b:46:a2:9f brd ff:ff:ff:ff:ff:ff
 3: wlp59s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc mq state DOWN mode DORMANT group default qlen 1000
-    link/ether 00:28:f8:aa:b4:64 brd ff:ff:ff:ff:ff:ff' }
+    link/ether 00:28:f8:aa:b4:64 brd ff:ff:ff:ff:ff:ff'
+    end
+
     let(:connection) { described_class.new }
 
     before do
